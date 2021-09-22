754cc62ab2d6417a9b2b71f74371a3634b326db7
RedPlus <redplus@redplus.net>
2021-09-23 06:46:45 +0900

    .NET로 기본 CRUD 기능 생성 후 테스트

---
+++ Hawaso/Hawaso/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Hawaso.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hawaso.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
