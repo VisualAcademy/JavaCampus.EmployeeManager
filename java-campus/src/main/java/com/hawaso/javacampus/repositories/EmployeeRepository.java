61dee911d3d59a88903e83fc27cd1b4af923049e
VisualAcademy <redplus@redplus.net>
2021-09-24 04:18:10 +0900

    EmployeeRepository

--- java-campus/src/main/java/com/hawaso/javacampus/repositories/EmployeeRepository.java
+++ java-campus/src/main/java/com/hawaso/javacampus/repositories/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.hawaso.javacampus.repositories;
 
+import java.util.List;
+
 import com.hawaso.javacampus.models.Employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
+    public List<Employee> findByOrderByIdDesc(); 
+    public List<Employee> findByOrderByFirstNameAsc(); 
+    public List<Employee> findAllByOrderByLastNameAsc(); 
+    public List<Employee> findByFirstNameContainsOrLastNameContainingIgnoreCase(String firstName, String lastName); 
 }
