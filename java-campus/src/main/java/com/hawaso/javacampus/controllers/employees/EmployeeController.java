fc6f3d8a0693af8b2958fdf836fab66899befa11
VisualAcademy <redplus@redplus.net>
2021-09-26 07:52:47 +0900

    입력 폼 구성 및 입력 처리 코드 작성 후 입력과 출력 테스트

--- java-campus/src/main/java/com/hawaso/javacampus/controllers/employees/EmployeeController.java
+++ java-campus/src/main/java/com/hawaso/javacampus/controllers/employees/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.hawaso.javacampus.controllers.employees;
 
+import com.hawaso.javacampus.models.employees.Employee;
 import com.hawaso.javacampus.services.employees.EmployeeService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,7 +27,16 @@ public class EmployeeController {
     }
     
     @GetMapping(value = "/create")
-    public String create() {
+    public String create(Model model) {
+
+        model.addAttribute("employee", new Employee());
+
         return "views/employees/create";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("employee") Employee model) {
+        _service.save(model);
+        return "redirect:/employee/list";
     }
 }
